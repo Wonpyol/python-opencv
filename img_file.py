3d2846c3231a74730d388c88bc99153ba022f7bc
wonpyolee <wonpyo.lee@monolith.co.kr>
2021-03-22 18:10:05 +0900

    이미지 입출력 동영상파일 읽기

---
+++ img_file.py
@@ -0,0 +1,19 @@
+import cv2
+
+img_file = "sameple_image/prod.png"
+save_file = "sameple_image/prod_gray.png"
+# mode flag
+# cv2.IMREAD_COLOR : BGR 컬러로 읽기, 기본 값
+# cv2.IMREAD_UNCHANGED : 파일 그래로 읽기
+# cv2.IMREAD_GRAYSCALE : 흑백으로 읽기
+
+img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
+
+if img is not None:
+    cv2.imshow('IMG', img)
+    cv2.imwrite(save_file, img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+else:
+    print("No image File" )
+
